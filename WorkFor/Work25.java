c8528e7f1ede4c869cc87c6fa3f230191df2e294
MrWinRock <mrwinrock11@gmail.com>
2023-08-08 11:47:47 +0700

    edit work for

--- WorkFor/Work25.java
+++ WorkFor/Work25.java
@@ -1,5 +1,11 @@
 public class Work25 {
     public static void main(String[] args) {
-        
+        for (int i = -9; i <= 0; i++) {
+            if (i % 2 == 0) {
+                System.out.print((i*-1) + " ," );
+            } else {
+                System.out.print(i + " ,");
+            }
+        }
     }
 }
